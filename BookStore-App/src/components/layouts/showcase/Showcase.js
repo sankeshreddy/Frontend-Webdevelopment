dd8a2aa257f2fa2c2af09dc3d4be0d7c244afdcb
sankeshreddy <sankeshreddy@gmail.com>
2023-12-17 23:12:30 +0530

    BookStore Application

---
+++ BookStore-App/src/components/layouts/showcase/Showcase.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './showcase.styles.css'
+import Navbar from "../navbar/Navbar";
+import SearchInputForm from "../../forms/searchInputForm/SearchInputForm";
+
+const Showcase = () => {
+    return (
+        <section className="showcase-container">
+            <Navbar darkTheme={false}/>
+            
+            <div className="overlay"></div>
+            <div className="showcase-content">
+                
+                <h2 className="text-primary">Welcome To BookStore🙏Buy quality books at cheaper price.</h2>
+                
+                
+                <SearchInputForm darkTheme={ true }/>
+            </div>
+        </section>
+    )
+}
+
+export default Showcase;
---
+++ BookStore-App/src/firebase/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDxS9Rz4w_cHXhIKADeZxWaea7TID73wY",
+  authDomain: "bookworm-2b417.firebaseapp.com",
+  projectId: "bookworm-2b417",
+  storageBucket: "bookworm-2b417.appspot.com",
+  messagingSenderId: "579431410856",
+  appId: "1:579431410856:web:999fdcc82879b60ad6269d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
