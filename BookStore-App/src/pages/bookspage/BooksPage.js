a97b454dc9f69455764469ace18a59fbf9d19b9c
sankeshreddy <sankeshreddy@gmail.com>
2024-01-27 11:04:32 +0530

    page which displays all the books details

--- BookStore-App/src/pages/bookspage/BooksPage.js
+++ BookStore-App/src/pages/bookspage/BooksPage.js
@@ -1,24 +1,26 @@
 import React from "react";
-import './booksPage.styles.css';
-import Navbar from '../../components/layouts/navbar/Navbar';
-import SearchInputForm from '../../components/forms/searchInputForm/SearchInputForm';
+import "./booksPage.styles.css";
+import Navbar from "../../components/layouts/navbar/Navbar";
+import SearchInputForm from "../../components/forms/searchInputForm/SearchInputForm";
 import ProductListingAll from "../../components/layouts/product-listing-all/ProductListingAll";
-import Footer from '../../components/layouts/footer/Footer';
+import Footer from "../../components/layouts/footer/Footer";
 
 const BooksPage = () => {
-    return (
-        <section>
-            <Navbar darkTheme={ true } />
+  return (
+    <section>
+      <Navbar darkTheme={true} />
 
-            <div className="search-container">
-                <h2>Find the <span className="text-primary">Books</span> that you want</h2>
-                <SearchInputForm darkTheme={ false } />
-            </div>
+      <div className="search-container">
+        <h2>
+          Find the <span className="text-primary">Books</span> that you want
+        </h2>
+        <SearchInputForm darkTheme={false} />
+      </div>
 
-            <ProductListingAll />
-            <Footer />
-        </section>
-    )
-}
+      <ProductListingAll />
+      <Footer />
+    </section>
+  );
+};
 
 export default BooksPage;
