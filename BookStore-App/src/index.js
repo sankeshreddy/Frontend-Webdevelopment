0e9e4137b5c3023dcd5a919c720dadde1ef0e301
sankeshreddy <sankeshreddy@gmail.com>
2024-01-27 11:49:11 +0530

    File to mount/rennder main react component on to root element

--- BookStore-App/src/index.js
+++ BookStore-App/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './App';
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById("app"));
 
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
