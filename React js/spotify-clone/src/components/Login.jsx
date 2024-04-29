aaac22d6f3abca07b538b72153fbf4a6ef8f8402
sankeshreddy <sankeshreddy@gmail.com>
2024-04-29 20:39:01 +0530

    Login component

---
+++ React js/spotify-clone/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styled from "styled-components";
+export default function Login(){
+    return(
+        <Container>
+            <img src="" alt="" />
+            <button>Connect Spotify</button>
+        </Container>
+    )
+}
+
+const Container = styled.div``;
