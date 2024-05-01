ccab2c315964a631497bd0b0dc0a082a9b334240
sankeshreddy <sankeshreddy@gmail.com>
2024-05-01 14:45:19 +0530

    app component

--- React js/spotify-clone/src/App.js
+++ React js/spotify-clone/src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Login";
+import { reducerCases } from "./utils/Constants";
+import { useStateProvider } from "./utils/StateProvider";
 export default function App(){
+  const[{token}, dispatch] = useStateProvider();
+  useEffect(()=>{
+    const hash = window.location.hash;
+    if(hash){
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      if(token){
+        dispatch({type: reducerCases.SET_TOKEN, token});
+      }
+    }
+
+  }
+);
   return(
     <div>
-      <Login/>
+      
     </div>
   )
 }
