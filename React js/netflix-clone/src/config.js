caeb67b0a874475ccd95180d721b1bf84c319e26
sankeshreddy <sankeshreddy@gmail.com>
2024-05-10 07:46:18 +0530

    netflix clone application

---
+++ React js/netflix-clone/src/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDFx0ePxY81eEhJ28XAEzKleAsxK8zYLkM",
+  authDomain: "netflix-clone-ee2b7.firebaseapp.com",
+  projectId: "netflix-clone-ee2b7",
+  storageBucket: "netflix-clone-ee2b7.appspot.com",
+  messagingSenderId: "327335001048",
+  appId: "1:327335001048:web:270174b42865c91ad610ad"
+};
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzE1NzA1MDhiMTEyNmI4YjM5M2ZjZmQ5ODE2NjQ3ZSIsInN1YiI6IjY2M2E1NmJmNjYxMWI0MDEyNzY5ZGM5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rN1VmsSPitCpKcNPASoEtG4YaplCxcrFIbAxBsmCL5w";
+export {firebaseConfig, TMDB_Access_Key};
