a98e2effd8a35bfd522e5d866ae8ccc23f7669e6
sankeshreddy <sankeshreddy@gmail.com>
2024-05-18 21:55:45 +0530

    App component

--- React js/gemini-clone/src/App.jsx
+++ React js/gemini-clone/src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 function App() {
   
 
   return (
     <>
-      <div>
-        
-        </div>
+      <Sidebar/>
     </>
   )
 }
