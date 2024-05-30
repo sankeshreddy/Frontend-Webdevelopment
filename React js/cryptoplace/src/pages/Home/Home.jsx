d98e2b7a1d34ed09108ff1481ab58b614ec947d5
sankeshreddy <sankeshreddy@gmail.com>
2024-05-30 17:27:57 +0530

    Home component

--- React js/cryptoplace/src/pages/Home/Home.jsx
+++ React js/cryptoplace/src/pages/Home/Home.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 import './Home.css'
 const Home = () => {
   return (
-    <div>
-        <h2>Home</h2>
+    <div className='home'>
+        <div className="hero">
+          <h1>Largest<br/> Crypto Marketplace</h1>
+          <p>Welcome to the world's largest cryptocurrency marketplace.
+            Signup to explore more about cryptos.
+          </p>
+          <form>
+            <input type="text" placeholder='Search crypto...' />
+            <button type='submit'>Search</button>
+          </form>
+        </div>
+        <div className="crypto-table">
+          <div className="table-layout">
+            <p>#</p>
+            <p>Coins</p>
+            <p>Price</p>
+            <p style={{textAlign:'center'}}>24hr Change</p>
+            <p className='market-cap'>Market Cap</p>
+          </div>
+        </div>
     </div>
   )
 }
