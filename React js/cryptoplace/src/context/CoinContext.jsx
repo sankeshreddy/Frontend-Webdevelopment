6b125b3c260a4e28996357d5e69ba59ffce7c723
sankeshreddy <sankeshreddy@gmail.com>
2024-06-02 18:19:45 +0530

    context file

--- React js/cryptoplace/src/context/CoinContext.jsx
+++ React js/cryptoplace/src/context/CoinContext.jsx
@@ -1,10 +1,27 @@
-const { createContext, useState } = require("react");
+const { createContext, useState, useEffect } = require("react");
 
 export const CoinContext = createContext();
 
 const CoinContextProvider = (props)=>{
     const [allCoin, setAllCoin] = useState([]);
-    
+    const [currency,setCurrency] = useState({
+        name:"usd",
+        symbol:"$"
+    })
+const fetchAllcoin = async ()=>{
+    const options = {
+  method: 'GET',
+  headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-EHnAD4K2uSLbUr7aK175RupR'}
+};
+
+fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options)
+  .then(response => response.json())
+  .then(response => setAllCoin(response))
+  .catch(err => console.error(err));
+}
+   useEffect(()=>{
+    fetchAllcoin();
+   },[]) 
 const contextValue = {
 
 }
