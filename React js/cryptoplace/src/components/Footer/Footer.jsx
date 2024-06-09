04351f5492d774a5fe21f7c85936c6322afb4e2a
sankeshreddy <sankeshreddy@gmail.com>
2024-06-09 21:43:05 +0530

    Footer component

--- React js/cryptoplace/src/components/Footer/Footer.jsx
+++ React js/cryptoplace/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <div>
-        
+        <p className='footer'>Copyright @ 2024, Cryptoplace - All Rights Reserved.</p>
     </div>
   )
 }
