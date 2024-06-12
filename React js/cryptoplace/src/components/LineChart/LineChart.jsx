4d814abdb04d1fe9dab3db25d43fbeb63a308421
sankeshreddy <sankeshreddy@gmail.com>
2024-06-12 14:47:13 +0530

    Line Chart component

---
+++ React js/cryptoplace/src/components/LineChart/LineChart.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import Chart from 'react-google-charts'
+
+const LineChart = () => {
+
+    const [data, setData] = useState([["Date","prices"]])
+  return (
+    <div>LineChart</div>
+  )
+}
+
+export default LineChart
