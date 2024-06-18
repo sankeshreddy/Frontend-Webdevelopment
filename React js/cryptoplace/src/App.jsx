fa879aa0c0e7fafcd6d8871fbc7faa5c498d75d3
sankeshreddy <sankeshreddy@gmail.com>
2024-06-18 18:16:56 +0530

    App Component

--- React js/cryptoplace/src/App.jsx
+++ React js/cryptoplace/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Coin from './pages/Coin/Coin'
 import Footer from './components/Footer/Footer'
@@ -14,7 +14,7 @@ const App = () => {
         <Route path='/coin/:coinId' element={<Coin/>}/>
       </Routes>
       <Footer/>
-      </div>
+    </div>
   )
 }
 
