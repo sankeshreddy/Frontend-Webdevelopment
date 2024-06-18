9c32a126522b67341ddb5df781142319021095b5
sankeshreddy <sankeshreddy@gmail.com>
2024-06-18 18:26:34 +0530

    main jsx file

--- React js/cryptoplace/src/main.jsx
+++ React js/cryptoplace/src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import CoinContextProvider from './context/CoinContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <CoinContextProvider>
-    <App />
-    </CoinContextProvider>
+      <CoinContextProvider>
+        <App />
+      </CoinContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
