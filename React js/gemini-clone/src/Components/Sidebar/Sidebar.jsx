093b6770d5afaa1342c96f7ebb1e6fa852dd11d4
sankeshreddy <sankeshreddy@gmail.com>
2024-06-27 22:56:40 +0530

    Sidebar Component

--- React js/gemini-clone/src/Components/Sidebar/Sidebar.jsx
+++ React js/gemini-clone/src/Components/Sidebar/Sidebar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Sidebar.css'
-
+import {assets} from '../../assets/assets'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-        
+        <div className="top">
+          <img className='menu' src={assets.menu_icon} alt=""/>
+          <div className="new-chat">
+            <img src={assets.plus_icon} alt="" />
+            <p>New Chat</p>
+          </div>
+        </div>
+        <div className="bottom">
+
+        </div>
 
     </div>
   )
