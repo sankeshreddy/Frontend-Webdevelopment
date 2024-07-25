7e536417905cef7bfc6ee1a5bf3396c49cc255bf
sankeshreddy <sankeshreddy@gmail.com>
2024-07-25 14:01:27 +0530

    gemini clone

--- React js/gemini-clone/src/App.jsx
+++ React js/gemini-clone/src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 
-function App() {
-  
-
+const App = () => {
   return (
-    <>
-      APP
-    </>
+    <div>App</div>
   )
 }
 
