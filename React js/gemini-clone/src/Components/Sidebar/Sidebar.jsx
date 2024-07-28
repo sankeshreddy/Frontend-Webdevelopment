c4b727fd48ca072eb8ee01c3c70b37df1c2e9666
sankeshreddy <sankeshreddy@gmail.com>
2024-07-28 16:25:22 +0530

    sidebar component

--- React js/gemini-clone/src/Components/Sidebar/Sidebar.jsx
+++ React js/gemini-clone/src/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import './Sidebar.css'
-import {assets} from '../../assets/assets'
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
 const Sidebar = () => {
   return (
-    <>
-    </>
-    
+    <div className="sidebar">
+      <div className="top">
+        <img src={assets.menu_icon} alt="" className="menu"/>
+      </div>
+      <div className="bottm"></div>
+    </div>
   );
 };
 
