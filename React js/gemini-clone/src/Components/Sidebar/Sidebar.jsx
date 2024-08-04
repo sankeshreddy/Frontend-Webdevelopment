7007735b6afa79b2bcb4fb0105056afce0dc85d7
sankeshreddy <sankeshreddy@gmail.com>
2024-08-04 19:51:12 +0530

    sidebar component

--- React js/gemini-clone/src/Components/Sidebar/Sidebar.jsx
+++ React js/gemini-clone/src/Components/Sidebar/Sidebar.jsx
@@ -5,9 +5,18 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <img src={assets.menu_icon} alt="" className="menu"/>
+        <img src={assets.menu_icon} alt="" className="menu" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" className="new-chat" />
+          <p>New Chat</p>
+        </div>
+        <div className="recent">
+          <p className="recent-title">
+            
+          </p>
+        </div>
       </div>
-      <div className="bottm"></div>
+      <div className="bottom"></div>
     </div>
   );
 };
